ce4a69acac4db25bb0ea3f453eeb7b4c08d10bf1
root <root@8ef07dfb35ae>
2025-06-24 20:27:51 +0000

    Frontend setup and components update

--- src/HomePage/Hero.jsx
+++ src/HomePage/Hero.jsx
@@ -3,7 +3,7 @@ import images from '../assets/images';
 
 const Hero = () => {
   return (
-    <div id="Hero_1" className="relative overflow-hidden bg-gradient-to-r from-blue-700 to-blue-800 py-32">
+    <div id="Hero_1" className="relative overflow-hidden bg-gradient-to-r from-red-500 to-red-700 py-32">
       <div className="absolute inset-0">
         <svg className="absolute left-0 top-0 opacity-20" viewBox="0 0 200 200">
           <path fill="#FFFFFF" d="M42.7,-73.2C55.9,-67.3,67.7,-57.1,75.6,-44.2C83.5,-31.3,87.6,-15.7,86.8,-0.4C86.1,14.8,80.5,29.6,72.5,42.9C64.5,56.2,54,68,41.2,74.4C28.4,80.8,14.2,81.8,-0.7,83C-15.6,84.2,-31.2,85.6,-44.3,79.7C-57.4,73.8,-68,60.7,-74.9,46.1C-81.8,31.4,-85,15.7,-84.7,0.2C-84.4,-15.4,-80.6,-30.8,-73.1,-44.2C-65.6,-57.6,-54.5,-69,-41.4,-74.8C-28.3,-80.6,-14.2,-80.8,0.6,-81.8C15.3,-82.8,30.6,-84.6,42.7,-73.2Z" transform="translate(100 100)" />
